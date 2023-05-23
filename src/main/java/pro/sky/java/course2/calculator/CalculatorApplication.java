e9709744d7450992b00f31b0777e2d0d4014459d
Evgenii.Pichugov <eugepic@yandex.ru>
2023-05-23 21:22:46 +0300

    выполнил дз

--- src/main/java/pro/sky/java/course2/calculator/CalculatorApplication.java
+++ src/main/java/pro/sky/java/course2/calculator/CalculatorApplication.java
@@ -1,4 +1,4 @@
-package pro.sky.java.course2.homework24;
+package pro.sky.java.course2.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/pro/sky/java/course2/calculator/controller/FirstController.java
@@ -0,0 +1,40 @@
+package pro.sky.java.course2.calculator.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+@RequestMapping(path = "/calculator")
+@RestController
+public class FirstController {
+
+    private final CalculateService calculateService;
+    public FirstController(CalculateService calculateService) {
+        this.calculateService = calculateService;
+    }
+    @GetMapping
+    public String helloCalculator() {
+        return calculateService.helloCalculator();
+    }
+    @GetMapping(path = "/plus")
+    public String calculateSum(@RequestParam(value = "num1", required = false) Integer num1, @RequestParam(value = "num2", required = false) Integer num2) {
+
+        return calculateService.calculateSum(num1, num2);
+    }
+    @GetMapping(path = "/minus")
+    public String calculateDifference(@RequestParam(value = "num1", required = false) Integer num1, @RequestParam(value = "num2", required = false) Integer num2) {
+        return calculateService.calculateDifference(num1, num2);
+    }
+
+    @GetMapping(path = "/multiply")
+    public String calculateProduct(@RequestParam(value = "num1", required = false) Integer num1, @RequestParam(value = "num2", required = false) Integer num2) {
+        return calculateService.calculateProduct(num1, num2);
+    }
+
+    @GetMapping(path = "/divide")
+    public String calculateQuotient(@RequestParam(value = "num1", required = false) Integer num1, @RequestParam(value = "num2", required = false) Integer num2) {
+        return calculateService.calculateQuotient(num1, num2);
+    }
+
+
+}
